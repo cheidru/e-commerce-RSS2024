baf535ef9e96fc31a40a0f149a4ad78c5bc117cc
Anzhalika Turlak <293852647@mail.ru>
2024-04-30 21:20:12 +0300

    feat: add ESLinter and airbnb

--- e-commerce/.eslintrc.cjs
+++ e-commerce/.eslintrc.cjs
@@ -1,24 +1,34 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2020": true
     },
     "extends": [
+        "airbnb" ,
+        "airbnb-typescript" ,
+        "airbnb/hooks",
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
     ],
     "overrides": [
+    ],
+    "ignorePatterns": [
+        'dist', '.eslintrc.cjs', 'vite.config.ts'
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "module",
+        "project": './tsconfig.json',
+        "tsconfigRootDir": __dirname,
     },
     "plugins": [
         "react",
         "@typescript-eslint"
     ],
     "rules": {
+        /*'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],*/
+        'react/react-in-jsx-scope': 0,
     }
 }
