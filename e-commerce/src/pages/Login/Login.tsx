ab61d84d0c554893dbe8f3e9ae09bdc58a57d86f
Anzhalika Turlak <293852647@mail.ru>
2024-05-04 19:58:01 +0300

    feat: add router

---
+++ e-commerce/src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <>
+      <h1>Async Store</h1>
+      <form>
+        <input type="text" placeholder="Mail" />
+        <input type="password" placeholder="Password" />
+      </form>
+    </>
+  );
+}
+
+export default Login;
