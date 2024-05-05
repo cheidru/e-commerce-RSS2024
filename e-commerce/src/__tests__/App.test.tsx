b849ebaafd94cc906ccf61d1f69cbd383eab4f93
Anzhalika Turlak <293852647@mail.ru>
2024-05-05 20:29:52 +0300

    fix: file test

--- e-commerce/src/__tests__/App.test.tsx
+++ e-commerce/src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from '../App/App';
 
 test('demo', () => {
   expect(true).toBe(true);
