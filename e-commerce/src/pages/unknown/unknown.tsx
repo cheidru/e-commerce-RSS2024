699e9487c8fecc6408554ad5abddcd4875aed5e5
Igor Checheurov <chei@yandex.ru>
2024-05-10 18:33:12 +0300

    feat: add modal and powerlayer componets

--- e-commerce/src/pages/unknown/unknown.tsx
+++ e-commerce/src/pages/unknown/unknown.tsx
@@ -1,7 +1,14 @@
+import Modal from '../../components/modal/modal';
 import './unknown.scss';
 
 function Unknown() {
-  return <h2 className="unknown">404</h2>;
+  return (
+    <Modal
+      displayNoneFlex={{ display: 'flex' }}
+      headerText="Error 404"
+      modalText="The page can not be found"
+    />
+  );
 }
 
 export default Unknown;
