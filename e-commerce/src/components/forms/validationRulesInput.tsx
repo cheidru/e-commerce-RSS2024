52752f1fc8e8d5abbe3c5600ae44f8d063412eef
CerBeer <ocelopp@gmail.com>
2024-05-13 00:53:33 +0300

    feat:on register page move the requared field check before other checks

--- e-commerce/src/components/forms/validationRulesInput.tsx
+++ e-commerce/src/components/forms/validationRulesInput.tsx
@@ -39,36 +39,36 @@ export const ValidationSchemaInputRegister = yup
   .object({
     email: yup
       .string() // RSS-ECOMM-2_01
+      .required('Email is required')
       .test(
         'no-spaces',
         'Email must not contain spaces',
         (value) => !/\s/.test(value || '')
       )
-      .email('Email must be properly formatted (e.g., example@email.com)')
-      // .trim('Email must not contain leading or trailing whitespace') // если изменят требования на удаление пробелов - пусть побудет здесь
-      .required('Email is required'),
+      .email('Email must be properly formatted (e.g., example@email.com)'),
+    // .trim('Email must not contain leading or trailing whitespace') // если изменят требования на удаление пробелов - пусть побудет здесь
     password: yup
       .string() // RSS-ECOMM-2_01
+      .required('Password is required')
       .min(8, 'Password must be at least 8 characters long')
       .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
       .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
       .matches(/\d/, 'Password must contain at least one digit')
-      .trim('Password must not contain leading or trailing whitespace')
-      .required('Password is required'),
+      .trim('Password must not contain leading or trailing whitespace'),
     firstName: yup
       .string() // RSS-ECOMM-2_09:
+      .required('First name is required')
       .matches(
         /^[A-Za-z]+$/,
         'First name must contain at least one character and no special characters or numbers'
-      )
-      .required('First name is required'),
+      ),
     lastName: yup
       .string() // RSS-ECOMM-2_09:
+      .required('Last name is required')
       .matches(
         /^[A-Za-z]+$/,
         'Last name must contain at least one character and no special characters or numbers'
-      )
-      .required('Last name is required'),
+      ),
     dateOfBirth: yup
       .date() // RSS-ECOMM-2_09:
       .max(minimumDateOfBirth, 'You must be at least 13 years old')
@@ -79,25 +79,25 @@ export const ValidationSchemaInputRegister = yup
         street: yup.string().required('Street is required'),
         city: yup
           .string() // RSS-ECOMM-2_09:
+          .required('City is required')
           .matches(
             /^[A-Za-z\s]+$/,
             'City must contain at least one character and no special characters or numbers'
-          )
-          .required('City is required'),
+          ),
         postalCode: yup
           .string() // RSS-ECOMM-2_09: ?????????????????????????????????? 12345 или A1B 2C3 для США и Канады
+          .required('Postal code is required')
           .matches(
             /^\d{5}(-\d{4})?$/,
             'Postal code must follow the format for the country'
-          )
-          .required('Postal code is required'),
+          ),
         country: yup
           .string() // RSS-ECOMM-2_09: из предопределенного списка или поля автозаполнения. e-commerce - setting project - area
+          .required('Country is required')
           .oneOf(
             ['USA', 'Canada', 'Mexico'], // e-commerce - setting project - area
             'Must be a valid country from the predefined list'
-          )
-          .required('Country is required'),
+          ),
       })
       .required('Address is required'),
   })
