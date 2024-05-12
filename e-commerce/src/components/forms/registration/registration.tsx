3e270604f2d56a750eb096f86b5d02008f274932
CerBeer <ocelopp@gmail.com>
2024-05-13 00:42:16 +0300

    feat:align input fields on registration page

--- e-commerce/src/components/forms/registration/registration.tsx
+++ e-commerce/src/components/forms/registration/registration.tsx
@@ -39,61 +39,69 @@ function RegistrationForm(): React.ReactElement {
     <form className="form__registration form" onSubmit={handleSubmit(onSubmit)}>
       <legend>Registration</legend>
 
-      <div className="input-wrapper">
-        <label htmlFor="firstName">
-          first Name:
-          <input
-            id="firstName"
-            type="text"
-            placeholder={placeholder.firstName}
-            className="form__refistration-firstName input-text"
-            /* eslint-disable react/jsx-props-no-spreading */
-            {...register('firstName', { onBlur: () => trigger('firstName') })}
-          />
-          {errors.firstName && (
-            <div className="input-error">{errors.firstName.message}</div>
-          )}
-        </label>
-        <label htmlFor="lastName">
-          last Name:
-          <input
-            id="lastName"
-            type="text"
-            placeholder={placeholder.lastName}
-            className="form__refistration-lastName input-text"
-            /* eslint-disable react/jsx-props-no-spreading */
-            {...register('lastName', { onBlur: () => trigger('lastName') })}
-          />
-          {errors.lastName && (
-            <div className="input-error">{errors.lastName.message}</div>
-          )}
-        </label>
+      <div className="input-wrapper-line">
+        <div className="input-wrapper-names">
+          <label htmlFor="firstName">
+            first Name:
+            <input
+              id="firstName"
+              type="text"
+              placeholder={placeholder.firstName}
+              className="form__registration-firstName input-text"
+              /* eslint-disable react/jsx-props-no-spreading */
+              {...register('firstName', { onBlur: () => trigger('firstName') })}
+            />
+            {errors.firstName && (
+              <div className="input-error">{errors.firstName.message}</div>
+            )}
+          </label>
+        </div>
 
-        <label htmlFor="dateOfBirth">
-          Date of Birth:
-          <input
-            id="dateOfBirth"
-            type="date"
-            className="form__refistration-login input-text"
-            /* eslint-disable react/jsx-props-no-spreading */
-            {...register('dateOfBirth', {
-              onBlur: () => trigger('dateOfBirth'),
-            })}
-          />
-          {errors.dateOfBirth && (
-            <div className="input-error">{errors.dateOfBirth.message}</div>
-          )}
-        </label>
+        <div className="input-wrapper-names">
+          <label htmlFor="lastName">
+            last Name:
+            <input
+              id="lastName"
+              type="text"
+              placeholder={placeholder.lastName}
+              className="form__registration-lastName input-text"
+              /* eslint-disable react/jsx-props-no-spreading */
+              {...register('lastName', { onBlur: () => trigger('lastName') })}
+            />
+            {errors.lastName && (
+              <div className="input-error">{errors.lastName.message}</div>
+            )}
+          </label>
+        </div>
+
+        <div className="input-wrapper-names">
+          <label htmlFor="dateOfBirth">
+            Date of Birth:
+            <input
+              id="dateOfBirth"
+              type="date"
+              className="form__registration-login input-text"
+              /* eslint-disable react/jsx-props-no-spreading */
+              {...register('dateOfBirth', {
+                onBlur: () => trigger('dateOfBirth'),
+              })}
+            />
+            {errors.dateOfBirth && (
+              <div className="input-error">{errors.dateOfBirth.message}</div>
+            )}
+          </label>
+        </div>
       </div>
 
       <div className="registration-adress">
+        <div>Shipping address</div>
         <div className="input-wrapper">
           <label htmlFor="address.street">
-            Strit
+            Street
             <input
               id="address.street"
               type="text"
-              className="form__refistration-adress input-text"
+              className="form__registration-adress input-text"
               /* eslint-disable react/jsx-props-no-spreading */
               {...register('address.street', {
                 onBlur: () => trigger('address.street'),
@@ -104,13 +112,14 @@ function RegistrationForm(): React.ReactElement {
             <div className="input-error">{errors.address.street.message}</div>
           )}
         </div>
+
         <div className="input-wrapper">
           <label htmlFor="address.city">
             City
             <input
               id="address.city"
               type="text"
-              className="form__refistration-adress input-text"
+              className="form__registration-adress input-text"
               /* eslint-disable react/jsx-props-no-spreading */
               {...register('address.city', {
                 onBlur: () => trigger('address.city'),
@@ -121,13 +130,14 @@ function RegistrationForm(): React.ReactElement {
             <div className="input-error">{errors.address.city.message}</div>
           )}
         </div>
+
         <div className="input-wrapper">
           <label htmlFor="address.postalCode">
             POST Code
             <input
               id="address.postalCode"
               type="text"
-              className="form__refistration-adress input-text"
+              className="form__registration-adress input-text"
               /* eslint-disable react/jsx-props-no-spreading */
               {...register('address.postalCode', {
                 onBlur: () => trigger('address.postalCode'),
@@ -146,7 +156,7 @@ function RegistrationForm(): React.ReactElement {
             <input
               id="address.country"
               type="text"
-              className="form__refistration-adress input-text"
+              className="form__registration-adress input-text"
               /* eslint-disable react/jsx-props-no-spreading */
               {...register('address.country', {
                 onBlur: () => trigger('address.country'),
@@ -159,52 +169,57 @@ function RegistrationForm(): React.ReactElement {
         </div>
       </div>
 
-      <div className="input-wrapper">
-        <label htmlFor="email">
-          Email:
-          <input
-            id="email"
-            type="email"
-            className="form__refistration-email input-text"
-            /* eslint-disable react/jsx-props-no-spreading */
-            {...register('email', { onBlur: () => trigger('email') })}
-          />
-          {errors.email && (
-            <div className="input-error">{errors.email.message}</div>
-          )}
-        </label>
+      <div className="input-wrapper-line">
+        <div className="input-wrapper">
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              type="email"
+              className="form__registration-email input-text"
+              /* eslint-disable react/jsx-props-no-spreading */
+              {...register('email', { onBlur: () => trigger('email') })}
+            />
+            {errors.email && (
+              <div className="input-error">{errors.email.message}</div>
+            )}
+          </label>
+        </div>
 
-        <label htmlFor="password">
-          Password:
-          <input
-            id="password"
-            type={showPassword ? 'text' : 'password'}
-            className="form__refistration-password input-text"
-            /* eslint-disable react/jsx-props-no-spreading */
-            {...register('password', { onBlur: () => trigger('password') })}
-          />
-        </label>
-        {errors.password && (
-          <div className="input-error">{errors.password.message}</div>
-        )}
-        <button
-          type="button"
-          className="btn-show"
-          onClick={() => setShowPassword(!showPassword)}
-        >
-          {showPassword ? 'Hide' : 'Show'}
-        </button>
+        <div className="input-wrapper">
+          <label htmlFor="password">
+            Password:
+            <input
+              id="password"
+              type={showPassword ? 'text' : 'password'}
+              className="form__registration-password input-text"
+              /* eslint-disable react/jsx-props-no-spreading */
+              {...register('password', { onBlur: () => trigger('password') })}
+            />
+          </label>
+          {errors.password && (
+            <div className="input-error">{errors.password.message}</div>
+          )}
+          <button
+            type="button"
+            className="btn-show"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? 'Hide' : 'Show'}
+          </button>
+        </div>
       </div>
-      <div className="input-wrapper">
+
+      <div className="input-wrapper-btn">
         <button
           type="submit"
           className="btn-submit"
           disabled={isSubmitDisabled}
         >
-          Login
+          Register
         </button>
       </div>
-      <div className="input-wrapper link-box">
+      <div className="input-wrapper-link link-box">
         <span className="link-text">
           If you have an account, you can logIn here
         </span>
