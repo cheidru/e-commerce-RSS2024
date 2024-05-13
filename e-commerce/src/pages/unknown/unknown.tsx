336c5c86de249faddc5f4b241e8fbd7d4237b74f
Anzhelika Turlak <70901455+Anzhelika007@users.noreply.github.com>
2024-05-13 20:21:46 +0300

    Revert "Error page404"

--- e-commerce/src/pages/unknown/unknown.tsx
+++ e-commerce/src/pages/unknown/unknown.tsx
@@ -1,30 +1,7 @@
-import { useNavigate } from 'react-router-dom';
-import bed from '../../assets/img/icons/bed.svg';
+import './unknown.scss';
 
 function Unknown() {
-  const navigate = useNavigate();
-  return (
-    <div className="unknown">
-      <div className="unknown-title-wrapper">
-        <img src={bed} alt="title-img" className="unknown-img" />
-        <p className="unknown-title">404</p>
-      </div>
-      <div className="unknown-wrapper">
-        <p className="unknown-text">The page is not found</p>
-        <div className="unknown-btn-wrapper">
-          <button
-            type="button" className="form__call-btn"
-            onClick={() => navigate('/', { replace: true })}
-          >
-            Go to Home
-          </button>
-          <button type="button" className="form__call-btn" onClick={() => navigate(-1)}>
-            Go Back
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+  return <h2 className="unknown">404</h2>;
 }
 
 export default Unknown;
