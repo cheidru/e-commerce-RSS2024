574bafe286a5b0a2a25f31931bd119330fea4fd2
Igor Checheurov <chei@yandex.ru>
2024-05-28 23:12:58 +0300

    feat: add and temporary comment link to userProfile foem on profile page

--- e-commerce/src/pages/profile/profile.tsx
+++ e-commerce/src/pages/profile/profile.tsx
@@ -1,8 +1,12 @@
+// import UserProfile from '../../components/forms/userProfile/userProfile';
 import './profile.scss';
 import UnderConstruction from '../../components/stubs/underconstruction';
 
 function Profile() {
   return (
+    // <section>
+    //   <UserProfile />
+    // </section>
     <>
       <h2 className="profile free-page">Profile</h2>
       <UnderConstruction />
